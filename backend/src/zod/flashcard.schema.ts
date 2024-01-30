cf500d82e3d80df6e4697db308688e84e406d56b
MohammedAlmanea <MohammedAlmanea@outlook.com>
2024-01-30 23:09:04 +0300

    Updated Flashcard zod schema

--- backend/src/zod/flashcard.schema.ts
+++ backend/src/zod/flashcard.schema.ts
@@ -5,4 +5,12 @@ export const FlashcardSchema = z.object({
   back: z.string().describe("The answer to the question")
 });
 
+export const updateFlashcardSchema = z.object({
+  front: z.string(),
+  back: z.string(),
+  resourceId: z.string(),
+  grade: z.number().min(0).max(5),
+});
+
 export type Flashcard = z.infer<typeof FlashcardSchema>;
+
