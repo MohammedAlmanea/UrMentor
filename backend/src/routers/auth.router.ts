bcd4e22a77ff6b120041eef3e7f9e50fc19c7f52
MohammedAlmanea <MohammedAlmanea@outlook.com>
2024-02-08 16:25:41 +0300

    Created logout route

--- backend/src/routers/auth.router.ts
+++ backend/src/routers/auth.router.ts
@@ -1,24 +1,34 @@
-import express from 'express';
-import passport from 'passport';
+import express, { Request, Response } from 'express';
+import passport, { session } from 'passport';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { User } from '@prisma/client';
+import { verifyToken } from '../middleware/jwt';
 
 dotenv.config();
 
 const router = express.Router();
 
 // auth logout
-router.get('/logout', (req, res) => {
-  // req.logout();
-  res.redirect('/');
+router.get('/logout', (req: Request, res: Response) => {
+  try {
+    res
+      .cookie('jwt', '', { maxAge: 0, httpOnly: true })
+      res.status(200).json("Logout is complete the token is removed")
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+router.get('/verify', verifyToken, (req,res) => {
+  res.status(200).send('Token is valid');
+})
 
 // auth with google+
 router.get(
   '/google',
   passport.authenticate('google', {
-    scope: ['profile','email'],
+    scope: ['profile', 'email'],
   })
 );
 
@@ -26,25 +36,27 @@ router.get(
 // hand control to passport to use code to grab profile info
 router.get(
   '/google/redirect',
-  passport.authenticate('google' ,{failureRedirect: 'http://localhost:5173/'  , session:false} ),
-  (req, res) => {
+  passport.authenticate('google', {
+    failureRedirect: 'http://localhost:5173/',
+    session: false,
+  }),
+  (req: Request, res: Response) => {
     // redirect to frontend page
     // res.redirect('http://localhost:3000/')
     if (req.user) {
       const user = req.user as User;
       const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string);
-      console.log("jwt token created");
-      
+      console.log('jwt token created');
+
       res.cookie('jwt', token, {
         httpOnly: true,
         // secure: true
       });
       console.log(user);
       console.log(token);
-      
-    //   res.redirect('/profile');
-     res.redirect('http://localhost:5173/dashboard')
 
+      //   res.redirect('/profile');
+      res.redirect('http://localhost:5173/dashboard');
     }
   }
 );
